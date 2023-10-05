471e4d54249517d6012742fc77136bdaf6098d25
yangdi0710 <phanlengocduy07102000@gmail.com>
2023-10-05 23:27:37 +0700

    G9 commit 11PM 05/10

--- project/tiktok/src/App.js
+++ project/tiktok/src/App.js
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 // function App() {
 //   const [counter, setCounter] = useState(1)
@@ -50,30 +50,70 @@
 
 
 
-import { useState } from "react";
+// import { useState } from "react";
 
-function App() {
-  const [info, setInfo] = useState({
-    name: "Duy",
-    age: 23,
-    city: "HCM"
-  }
-  )
+// function App() {
+//   const [info, setInfo] = useState({
+//     name: "Duy",
+//     age: 23,
+//     city: "HCM"
+//   }
+//   )
 
-  const handleUpdate = function(){
-      setInfo({
-        ...info,
-        addInfo: "Info"
-      }
-    )
-  }
+//   const handleUpdate = function(){
+//       setInfo({
+//         ...info,
+//         addInfo: "Info"
+//       }
+//     )
+//   }
+
+//   return (
+//     <div className="App" style={{marginLeft: 20}}>
+//       <h2>{JSON.stringify(info)}</h2>
+//       <button onClick={handleUpdate}>Increase</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// const gifts = [
+//   'CPU',
+//   'Memory',
+//   'GPU'
+// ]
+
+// function App () {
+
+//   const [gift, setGift] = useState()
+
+//   const randomGift = () => {
+//     const index = Math.floor(Math.random() * gifts.length)
+//     setGift(gifts[index])
+//   }
+
+//   return (
+//     <div style={{padding: 32}}>
+//       <h1>{gift || "Chưa có phần thưởng"}</h1>
+//       <button onClick={randomGift}>Lấy phần thưởng</button>
+//     </div>
+//   )
+// }
+
+
+
+  
+
+function App () {
+
 
   return (
-    <div className="App" style={{marginLeft: 20}}>
-      <h2>{JSON.stringify(info)}</h2>
-      <button onClick={handleUpdate}>Increase</button>
+    <div style={{padding: 32}}>
+      
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
