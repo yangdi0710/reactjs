6209e1ee56972638325ea95aff8d7dcafb737a0c
yangdi0710 <phanlengocduy07102000@gmail.com>
2023-10-09 23:20:37 +0700

    G9 commit 11PM 09/10

--- project/tiktok/src/App.js
+++ project/tiktok/src/App.js
@@ -107,12 +107,74 @@ import { useState } from "react";
 
   
 
+// function App () {
+
+//   const [name, setName] = useState('')
+//   const [email, setEmail] = useState('')
+
+//   const handleSubmit = () => {
+//     // call API
+//     console.log({
+//       name,
+//       email
+//     });
+//   }
+
+//   return (
+//     <div style={{padding: 32}}>
+//       <input 
+//         value={name}
+//         onChange={e => setName(e.target.value)}
+//       />
+//       <br />
+//       <input 
+//         value={email}
+//         onChange={e => setEmail(e.target.value)}
+//       />
+//       <button onClick={handleSubmit}>register</button>
+//     </div>
+//   )
+// }
+
+
+// Respone from API
+
+const courses = [
+  {
+    id: 1,
+    name: 'HTML, CSS'
+  },
+  {
+    id: 2,
+    name: 'JavaScript'
+  },
+  {
+    id: 3,
+    name: 'React JS'
+  }
+]
+
 function App () {
 
+  const [checked, setChecked] = useState()
+
+  const handleSubmit = () => {
+    console.log(checked);
+  }
 
   return (
     <div style={{padding: 32}}>
-      
+      {courses.map(course => (
+        <div key={course.id}>
+          <input 
+            type="radio" 
+            checked = {checked === course.id}
+            onChange={() => setChecked(course.id)}
+          />
+          {course.name}
+        </div>
+      ))}
+      <button onClick={handleSubmit}>register</button>
     </div>
   )
 }
