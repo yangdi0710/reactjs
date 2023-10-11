eac4d8a3bf608331b21516c4734a6d2acf07d049
yangdi0710 <phanlengocduy07102000@gmail.com>
2023-10-11 16:36:18 +0700

    G9 commit 11/10 4PM

--- project/todolist/src/App.js
+++ project/todolist/src/App.js
@@ -1,16 +1,39 @@
 import { useState } from "react";
 
 function App() {
-  const [jobs, setJobs] = useState([])
+
+  const [job, setJob] = useState('')
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem('jobs'))
+    console.log(storageJobs);
+    return storageJobs ?? []
+  })
+  console.log(job);
+
+  const handleSubmit = () => {
+    setJobs(prev => {
+      const newJob = [...prev, job]
+
+      // Save to localstorage
+      const jsonJobs = JSON.stringify(newJob)
+      localStorage.setItem('jobs', jsonJobs)
+
+      return newJob
+    }) 
+    setJob('')
+  }
 
   return (
     <div style={{padding: 32}}>
-      <input />
-      <button >Add</button>
+      <input 
+        value={job}
+        onChange={e => setJob(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Add</button>
 
       <ul>
         {jobs.map((job, index) => (
-          <li key={index}></li>
+          <li key={index}>{job}</li>
         ))}
       </ul>
     </div>
