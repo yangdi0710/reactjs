9ae8f198b0e4cd2a519bff0f5489f6bc3e086701
yangdi0710 <phanlengocduy07102000@gmail.com>
2023-10-12 23:43:56 +0700

    G9 commit 11PM 12/10

--- project/useeffect/src/Content.js
+++ project/useeffect/src/Content.js
@@ -27,40 +27,100 @@ import { useEffect, useState } from "react"
 // useEffect(callback)
 // - Gọi callback mỗi khi component re-render
 // - Gọi callback sau khi component thêm element vào DOM
+
 // useEffect(callback, [])
-// useEffect(callback, [dependency])
+// - Chỉ gọi callback một lần sau khi component mounted
+
+// useEffect(callback, [dependencies])
+// - Callback sẽ được gọi lại mỗi khi dependencies (deps) thay đổi
 
 //----------------------------------------------------------------
 // 1. Callback luôn được gọi sau khi component mounted
+// 2. Cleanup luôn được gọi trước khi component unmounted
 
 
+const tabs = ['posts', 'comments', 'albums']
 
 function Content() {
 
     const [title, setTitle] = useState('')
     const [posts, setPosts] = useState([])
+    const [type, setType] = useState('posts')
+    const [ShowGoToTop, setShowGoToTop] = useState(false)
+
+    // console.log(type);
 
     useEffect(() => {
+        console.log('title changed');
         // document.title = title
 
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
             .then(res => res.json())
             . then(posts => {
                 setPosts(posts)
             })
-    })
+    }, [type])
+
+    useEffect(() => {
+
+        const handleScroll = () => {
+            if (window.scrollY >= 300){
+                //Show
+                setShowGoToTop(true)
+            } else {
+                //Hide
+                setShowGoToTop(false)
+            }
+
+            //setShowGoToTop(window.scrollY >= 200)
+        }
+
+        window.addEventListener('scroll', handleScroll)
+        // Cleanup function
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+            
+        }
+
+    }, [])
 
     return (
         <div>
+
+            {tabs.map(tab => (
+                <button 
+                    key={tab}
+                    style={type === tab ? {
+                        color: 'white',
+                        backgroundColor: '#000',
+                    } : {}}
+                    onClick={() => setType(tab)}
+                    
+                >
+                    {tab}
+                </button>
+            ))}
+
             <input 
                 value={title}
                 onChange={e => setTitle(e.target.value)}
             />
             <ul>
                 {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
+                    <li key={post.id}>{post.title || post.name}</li>
                 ))}
             </ul>
+            {ShowGoToTop && (
+                <button
+                    style={{
+                        position: 'fixed',
+                        bottom: 20,
+                        right: 20
+                    }}
+                >
+                    Go to top!
+                </button>
+            )}
         </div>
     )
 }
