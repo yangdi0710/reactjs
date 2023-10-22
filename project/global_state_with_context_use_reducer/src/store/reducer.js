d47d4f4f6fcde8945b67a85f52e4a57528901c91
yangdi0710 <phanlengocduy07102000@gmail.com>
2023-10-22 22:42:12 +0700

    g9 commit 22/10 11P<

---
+++ project/global_state_with_context_use_reducer/src/store/reducer.js
@@ -0,0 +1,14 @@
+
+const initState = {
+    todos: [],
+    todoInput: ''
+}
+
+function reducer(state, acion) {
+    switch(acion.type) {
+        
+    }
+}
+
+export { initState }
+export default reducer
